36a48773b8eed25d28429e7fcddb0de174244d49
JuliaSaxS <120261142+JuliaSaxS@users.noreply.github.com>
2023-07-18 14:29:18 +0300

    Added changes

--- HW_3/src/App.java
+++ HW_3/src/App.java
@@ -12,21 +12,21 @@ public class App {
         Student s1 = new Student("Юрий", "Баранов", 20, (long) 111);
         Student s2 = new Student("Соня", "Глазкова", 21, (long) 121);
         Student s3 = new Student("Елизавета", "Петрова", 22, (long) 211);
-        Student s4 = new Student("Николай", "Колоша", 20, (long) 221);
+        Student s4 = new Student("Николай", "Колоша", 20, (long) 121);
         Student s5 = new Student("Илья", "Горличенко", 21, (long) 111);
         Student s6 = new Student("Ольга", "Иванова", 23, (long) 121);
 
         List<Student> lisStud = new ArrayList<Student>();
-        lisStud.add(s1);
-        lisStud.add(s2);
-        lisStud.add(s3);
-        lisStud.add(s4);
-        StudentGroup group = new StudentGroup(lisStud, 23454);
+        listStud.add(s1);
+        listStud.add(s2);
+        listStud.add(s3);
+        listStud.add(s4);
+        StudentGroup group = new StudentGroup(listStud, 111);
 
         List<Student> lisStud2 = new ArrayList<Student>();
-        lisStud2.add(s5);
-        lisStud2.add(s6);
-        StudentGroup group2 = new StudentGroup(lisStud2, 15454);
+        listStud2.add(s5);
+        listStud2.add(s6);
+        StudentGroup group2 = new StudentGroup(listStud2, 121);
 
         StudentSteam team = new StudentSteam(2);
         team.addTeam(group);
