9f8409194a62599d70c8aae224782932058b1be9
JuliaSaxS <120261142+JuliaSaxS@users.noreply.github.com>
2023-07-18 15:58:38 +0300

    Added changes

--- HW_3/Main.java
+++ HW_3/Main.java
@@ -3,12 +3,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-
-/*
- * В методе `main` создается объект `stream` класса `StudentStream` с номеромпотока 1. 
- * Затем создаются три объекта `group1`, `group2`, `group3` класса`StudentGroup` с разным количеством студентов, 
- * и каждая группа добавляется всписок групп потока.
- */
  
  public class Main {
 
@@ -16,13 +10,11 @@ import java.util.List;
 
         StudentStream stream = new StudentStream(1);
 
+        StudentGroup group1 = new StudentGroup("Group 1", 20);
 
+        StudentGroup group2 = new StudentGroup("Group 2", 15);
 
-        StudentGroup group1 = new StudentGroup("1", 29);
-
-        StudentGroup group2 = new StudentGroup("2", 31);
-
-        StudentGroup group3 = new StudentGroup("3", 30);
+        StudentGroup group3 = new StudentGroup("Group 3", 10);
 
 
         stream.addGroup(group1);
